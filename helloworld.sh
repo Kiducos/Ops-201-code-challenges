190791c48a01e30915f7775bbfa4978ce4c7a831
Kiducos <kiducos@yahoo.com>
2023-08-11 00:09:56 +0000

    Commit file helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  helloworld.sh
+# Author:                       Kevin Ducos
+# Date of latest revision:      08/10/2023
+# Purpose:                      Ops Challenge - My First Bash Script 
+
+# Declaration of variables
+good="I am working perfectly!"
+
+# Declaration of functions
+
+
+# Main
+echo "hello world $good"
+
+
+
+# End 
