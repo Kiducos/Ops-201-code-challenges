16713b6a165fe4f71e4c852ffb6aed8cabcc8026
Kiducos <kiducos@yahoo.com>
2023-08-16 00:54:11 +0000

    Login history script - Function Example

---
+++ loghistory.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name:                  First Function - Log History 
+# Author:                       Kevin Ducos 
+# Date of latest revision:      08/15/2023
+# Purpose:                      Ops Challenge 03
+
+# Declaration of variables
+
+# Declaration of functions
+Login_History() { 
+    lastlog
+
+echo This is the login history 
+}
+
+# Main
+Login_History
+Login_History
+Login_History
+
+
+
+# End
